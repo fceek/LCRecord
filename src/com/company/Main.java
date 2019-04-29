ee53ec468010f127f94e52f24e8e58eab6ac84f0
fceek <fceek96@gmail.com>
2019-04-29 22:26:46 +0100

    daily practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -46,16 +46,82 @@ public class Main {
         try {
             ans = Integer.parseInt(result);
         } catch (NumberFormatException e) {
-            //
+            // who cares?
         }
         if ( (isPositive && ans > limit) || (!isPositive && ans > limit+1)) ans = 0;
         if (!isPositive) ans = -ans;
         return (int)ans;
     }
 
+    boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        int oriX = x;
+        int digit = 0;
+        int revX = 0;
+        while (x != 0) {
+            digit = x % 10;
+            x = x / 10;
+            revX = revX * 10 + digit;
+        }
+        return (revX == oriX);
+    }
+
+    int romanToInt(String s) {
+        int roman = 0;
+        for (int i = 0; i < s.length() ; i++) {
+            switch (s.charAt(i)) {
+                case 'M': roman+=1000; break;
+                case 'D': roman+=500; break;
+                case 'C':
+                    if ((i+1 < s.length()) && (s.charAt(i+1) == 'D')) {
+                        roman+=400;
+                        ++i;
+                        break;
+                    }
+                    if ((i+1 < s.length()) && (s.charAt(i+1) == 'M')) {
+                        roman+=900;
+                        ++i;
+                        break;
+                    }
+                    roman+=100;
+                    break;
+                case 'L': roman+=50; break;
+                case 'X':
+                    if ((i+1 < s.length()) && (s.charAt(i+1) == 'L')) {
+                        roman+=40;
+                        ++i;
+                        break;
+                    }
+                    if ((i+1 < s.length()) && (s.charAt(i+1) == 'C')) {
+                        roman+=90;
+                        ++i;
+                        break;
+                    }
+                    roman+=10;
+                    break;
+                case 'V': roman+=5; break;
+                case 'I':
+                    if ((i+1 < s.length()) && (s.charAt(i+1) == 'V')) {
+                        roman+=4;
+                        ++i;
+                        break;
+                    }
+                    if ((i+1 < s.length()) && (s.charAt(i+1) == 'X')) {
+                        roman+=9;
+                        ++i;
+                        break;
+                    }
+                    roman+=1;
+                    break;
+            }
+        }
+        return roman;
+    }
+
     public static void main(String[] args) {
         int[] a={2,7,11,15};
-        int solution = new Main().reverse(2147083649);
+        int solution = new Main().romanToInt("MCMXCIV");
         System.out.println(solution);
     }
 }
